8cf585f21c324abf3b98cb12d9e5a79949a979cc
Valnaevis <vgoyzuetat@ulasalle.edu.pe>
2023-07-03 00:36:45 -0500

    Configurations

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import { registerRootComponent } from 'expo';
-
+import React from 'react';
+import FavoritesComponent from './src/hooks/favoritesComponent';
 import App from './App';
+import { AppRegistry } from 'react-native';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+
+const Context = () => { //Para que el Context FUNCIONE
+    return (
+        <FavoritesComponent>
+            <App />
+        </FavoritesComponent>
+    );
+}
+
+AppRegistry.registerComponent('main', () => Context);
